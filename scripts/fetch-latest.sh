c81eb8b5e558ea6cb988fd69af6c0d11f06d2b56
Patrik Marin <patrik.marin@gmail.com>
2019-05-13 20:49:35 +0300

    Fix update script

--- scripts/fetch-latest.sh
+++ scripts/fetch-latest.sh
@@ -6,7 +6,7 @@ UPDATE_NOTES=`jq -r '.body' <<< $LATEST_RELEASE`
 CREATION_DATE=`jq -r '.created_at' <<< $LATEST_RELEASE`
 CURRENT_RELEASE=$(cat CURRENT-RELEASE)
 
-if [ $CURRENT_RELEASE == $RELEASE_NAME ];
+if [ "$CURRENT_RELEASE" == "$RELEASE_NAME" ] && [ "$1" != "-F" ];
 then
   echo "No new release found... exiting"
   exit 0
@@ -53,15 +53,15 @@ rm $RELEASE_NAME.tar.gz
 # Update RELEASE information
 echo -n $RELEASE_NAME > CURRENT-RELEASE
 
+sleep 5
+
 CURRENT_DIAGRAM_HASH=$(cat CURRENT-DIAGRAM)
 NEW_DIAGRAM_HASH=$(cat dist/diagram.bpmn.sha256)
 
 DIAGRAM_UPDATE=false
 
-if [ $CURRENT_DIAGRAM_HASH != $NEW_DIAGRAM_HASH ];
+if [ "$CURRENT_DIAGRAM_HASH" != "$NEW_DIAGRAM_HASH" ];
 then
-  sleep 5
-
   echo "Deploy new BPMN diagram..."
   DEPLOY_RESULT=`curl --silent -X POST \
     http://localhost:3000/deploy \
@@ -74,14 +74,16 @@ then
 fi
 
 # Send update message to API
-curl --silent -X POST \
+DEPLOY_RESULT=`curl --silent -X POST \
   http://localhost:3000/update-notes \
   -H "Authorization: $AUTH_TOKEN" \
   -H 'Content-Type: application/json' \
   -d '{
-	"date": "$CREATION_DATE",
-	"oldVersion": "${CURRENT_RELEASE:1}",
-	"newVersion": "${RELEASE_NAME:1}",
-	"updateNotes": "$UPDATE_NOTES",
-	"diagramUpdate": $DIAGRAM_UPDATE
-}'
+	"date": "'"$CREATION_DATE"'",
+	"oldVersion": "'"${CURRENT_RELEASE:1}"'",
+	"newVersion": "'"${RELEASE_NAME:1}"'",
+	"updateNotes": "'"$UPDATE_NOTES"'",
+	"diagramUpdate": '$DIAGRAM_UPDATE'
+}'` 
+
+echo $DEPLOY_RESULT
